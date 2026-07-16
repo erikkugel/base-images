b05d29edfae082246a9751bde0ba780559f9eabd
Ernest Kugel <erik.kugel@gmail.com>
2026-07-15 23:33:10 -0400

    Refactor to add schedule

--- .github/actions/slackware64_root_fs_archive/entrypoint.sh
+++ .github/actions/slackware64_root_fs_archive/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 cd slackware64
-export SOURCE="${GITHUB_WORKSPACE}/slackware64/slackware64-15.0"
-export TARGET="${GITHUB_WORKSPACE}/slackware64/base"
-./build
+SOURCE="${GITHUB_WORKSPACE}/slackware64/slackware64-15.0" TARGET="${GITHUB_WORKSPACE}/slackware64/base" ./build
